56cf1a1d2cd9e59ffd39a53fc0aa9a14afa50596
xiaoxiong <chenmu5241@163.com>
2018-05-23 11:24:23 +0800

    增加商城代码

---
+++ js/app.js
@@ -0,0 +1,56 @@
+(function(window,$){
+	window.baseUrl  = "/fmmapp/";
+	window.bgBaseUrl = "http://192.168.1.154:8080/fmmapp/";//全局记录服务器端地址
+	
+	//封装localStorage
+	window.myLocalStorage = {};
+	myLocalStorage.setItem = function(key,value){
+		if(value instanceof String){
+			localStorage.setItem(key,value)
+		}else{
+			localStorage.setItem(key,JSON.stringify(value))
+		}
+	}
+	
+	myLocalStorage.getItem = function(key){
+		var value = localStorage.getItem(key);
+		try{
+			value=JSON.parse(value)
+		}catch(e){
+		}
+		return value;
+	}
+	
+	//封装sessionStorage
+	window.mySessionStorage = {};
+	mySessionStorage.setItem = function(key,value){
+		if(value instanceof String){
+			sessionStorage.setItem(key,value)
+		}else{
+			sessionStorage.setItem(key,JSON.stringify(value))
+		}
+	}
+	
+	mySessionStorage.getItem = function(key){
+		var value = sessionStorage.getItem(key);
+		try{
+			value=JSON.parse(value)
+		}catch(e){
+			value = sessionStorage.getItem(key);
+		}
+		return value;
+	}
+    
+    
+    //请求拦截
+    $.ajaxSetup({
+        type: "POST",
+        beforeSend: function (evt, request, settings) {
+            request.url = bgBaseUrl + request.url;
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+        	mui.toast("请求数据异常！")
+        }
+    });
+
+}(window,jQuery))
